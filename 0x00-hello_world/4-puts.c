e9b8e230d8eba3eee1fa1b067d62c84a7d9b8986
OsarsStephen <osars213@gmail.com>
2022-09-08 18:54:34 -0700

    cisfun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  *main- Serves as the entry point for the program
- *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
+ *main- Serves as the entry point for the program
+ *   
+ *Return: By default returns zero if no erros otherwise returns non-zero value.
  *     */
 int main(void)
 {
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,15 @@
-#include <unistd.h>
-/*
- *  main-program entry point.
- *   
- *    Return: 1 on success.
+#include <stdio.h>
+/**
+ *  *main -is the entry point for the program.
+ *   *
+ *    *Return:zero if no error, Non-zero value  if errors.
  *     */
 int main(void)
 {
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-			return (1);
+		printf("Size of a char: %ld byte(s)\n", sizeof(char));
+			printf("Size of an int: %ld byte(s)\n", sizeof(int));
+				printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+					printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+						printf("Size of a float: %ld byte(s)\n", sizeof(float));
+							return (0);
 }
